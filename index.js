23ede157a625b3fe41d8dd12435e613c23275028
Thomas Kinsella <thomasjkinsella4@gmail.com>
2022-02-27 09:55:17 -0500

    commit tree b337776c87a08303d54457f7b68ed0d7e526628d

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+const employee = {
+    name : "Thomas",
+    streetAddress : "123 Main Rd"
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+     const newObj = {...obj};
+     newObj[key] = value;
+     return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = {...obj};
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+}
